46315d20bc2eaec65cb69f694a7b744cb82bd7d2
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2019-09-11 18:23:12 +0300

    innit commit

---
+++ app/src/main/java/com/ahmedghoul/Movie/Videos/Video.java
@@ -0,0 +1,15 @@
+package com.ahmedghoul.Movie.Videos;
+
+
+public class Video {
+
+    private String mVideoName;
+
+    public Video(String mVideoName) {
+        this.mVideoName = mVideoName;
+    }
+
+    public String getmVideoName() {
+        return mVideoName;
+    }
+}
